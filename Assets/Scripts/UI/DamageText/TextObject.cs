9ee50dba26e70156d8bef94ae85e2691351ecda5
Rope <roperaganmen88@gmail.com>
2026-03-13 17:44:39 +0900

    ダメージテキスト出現

--- Assets/Scripts/UI/DamageText/TextObject.cs
+++ Assets/Scripts/UI/DamageText/TextObject.cs
@@ -1,8 +1,21 @@
 using UnityEngine;
+using DG.Tweening;
+using Cysharp.Threading.Tasks;
+using TMPro;
+using System;
 
 public class TextObject : MonoBehaviour
 {
+    [SerializeField]
+    private RectTransform _rectTransform;
+
+    [SerializeField]
+    private TextMeshProUGUI _tmp;
+
     private TextObjectPool _pool;
+
+    private const float AppearTime = 0.4f;
+    private const float DisappearTime = 0.6f;
     public TextObjectPool Pool
     {
         get => _pool;
@@ -12,5 +25,23 @@ public class TextObject : MonoBehaviour
     public void Release()
     {
         Pool.ReturnToPool(this);
+    }
+
+    public async void Appear()
+    {
+        var spawnPos = _rectTransform.anchoredPosition.x;
+
+        _tmp.color = new Color(_tmp.color.r, _tmp.color.g, _tmp.color.b, 0);
+        await UniTask.WhenAll(
+            _tmp.DOFade(1f, AppearTime).ToUniTask(),
+            _rectTransform.DOAnchorPosX(spawnPos - 100, AppearTime).SetEase(Ease.OutQuad).ToUniTask()
+        );
+        await UniTask.Delay(TimeSpan.FromSeconds(0.5f));
+        await UniTask.WhenAll(
+            _tmp.DOFade(0f, DisappearTime).ToUniTask(),
+            _rectTransform.DOAnchorPosX(spawnPos - 200, DisappearTime).SetEase(Ease.InQuad).ToUniTask()
+        );
+
+        Release();
     }
 }
--- Assets/Scripts/UI/DamageText/TextObjectPool.cs
+++ Assets/Scripts/UI/DamageText/TextObjectPool.cs
@@ -1,6 +1,6 @@
 using System.Collections.Generic;
 using UnityEngine;
-
+using AndanteTribe.Utils.Unity;
 public class TextObjectPool : MonoBehaviour
 {
     [SerializeField]
@@ -8,6 +8,9 @@ public class TextObjectPool : MonoBehaviour
     
     [SerializeField]
     private TextObject objectToPool;
+
+    [SerializeField]
+    private Transform _canvasTransform;
 
     private Stack<TextObject> stack;
 
@@ -22,25 +25,28 @@ public class TextObjectPool : MonoBehaviour
         TextObject instance = null;
         for (int i = 0; i < _initSize; i++)
         {
-            instance = Instantiate(objectToPool);
+            instance = Instantiate(objectToPool, _canvasTransform);
             instance.Pool = this;
             instance.gameObject.SetActive(false);
             stack.Push(instance);
         }
     }
 
+    [Button("出現")]
     public TextObject GetPooledObject()
     {
         // プールの大きさが十分でない場合は、新しい PooledObjects をインスタンス化する
         if (stack.Count == 0)
         {
-            TextObject newInstance = Instantiate(objectToPool);
+            TextObject newInstance = Instantiate(objectToPool, _canvasTransform);
             newInstance.Pool = this;
+            newInstance.Appear();
             return newInstance;
         }
         // それ以外の場合は、リストから次のものをグラブする
         TextObject nextInstance = stack.Pop();
         nextInstance.gameObject.SetActive(true);
+        nextInstance.Appear();
         return nextInstance;
     }
 
