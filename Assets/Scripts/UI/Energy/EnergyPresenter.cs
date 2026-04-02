91e3d01654a7faf80037cdde7da9d3a2f68b621a
Rope <roperaganmen88@gmail.com>
2026-04-02 16:01:08 +0900

    fix: 終点移動を変更

--- Assets/Scripts/UI/Energy/EnergyPresenter.cs
+++ Assets/Scripts/UI/Energy/EnergyPresenter.cs
@@ -11,18 +11,23 @@ namespace UI.Energy
         /// <summary>
         /// 終点となるUIのRectTransform
         /// </summary>
-        public RectTransform rectTransform;
+        public RectTransform _destinationRect;
         
         /// <summary>
         /// 出現する位置のTransform
         /// </summary>
         public Vector3 emit;
-        
+
+        [SerializeField]
+        private Light _light;
+
         private const float AppearTime = 0.5f;
+        
+        private const float LightIntensity = 5f;
         
         public void SetPosition(RectTransform rect, Vector3 trans)
         {
-            rectTransform = rect;
+            _destinationRect = rect;
             emit = trans;
         }
 
@@ -30,18 +35,40 @@ namespace UI.Energy
         {
             // rectTransformのグローバルの位置をとってくる
             // これを終点とする
-            var destination = Camera.main.ScreenToWorldPoint(emit);
-            destination.Set(destination.x, destination.y, destination.z + 1f);
+
+            // Screen Space - Overlay の場合:
+            // _destinationRect.position はスクリーン座標なので、
+            // z にカメラからの距離（奥行き）を入れて ScreenToWorldPoint で変換する
+            var screenPos = _destinationRect.position;
+            screenPos.Set(screenPos.x, screenPos.y, 1f);
+            var destination = Camera.main.ScreenToWorldPoint(screenPos);
+            Debug.Log(destination);
 
             var start = emit;
             transform.position = start;
-            
+
             gameObject.SetActive(true);
+
+            Time.timeScale = 0.4f;
             
             // 移動し始める
-            await transform.DOMove(transform.position + new Vector3(Random.Range(-1f, 1f), Random.Range(-1f, 1f), Random.Range(-1f, 1f)), AppearTime).SetEase(Ease.OutCubic).AsyncWaitForCompletion();
+            transform.DOMove(
+                transform.position + new Vector3(Random.Range(-1f, 1f), Random.Range(0, 1f), Random.Range(-1f, 1f)),
+                AppearTime).SetEase(Ease.OutCubic).ToUniTask().Forget();
+
+
+            await _light.DOIntensity(LightIntensity, AppearTime / 2).SetEase(Ease.OutCubic);
+            await _light.DOIntensity(0, AppearTime / 2).SetEase(Ease.InQuad);
             
-            await transform.DOMove(destination, AppearTime).SetEase(Ease.InCubic).AsyncWaitForCompletion();
+            
+            // UIの位置に移動させるの、なんかダメなので無効
+            // await UniTask.WhenAll(
+            //     transform.DOMove(destination, AppearTime).SetEase(Ease.InCubic).ToUniTask(),
+            //     _light.DOIntensity(0, AppearTime).SetEase(Ease.InCubic).ToUniTask()
+            // );
+            
+            // エナジーの量をプレイヤーに増やす
+            
 
             gameObject.SetActive(false);
             Release();
