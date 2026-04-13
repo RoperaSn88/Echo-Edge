a42533aa7d485070e3657c605c8172bc497bf811
Rope <roperaganmen88@gmail.com>
2026-04-14 00:42:33 +0900

    feat: Addressablesと武器UI表示

--- Assets/Scripts/UI/Weapon/WeaponController.cs
+++ Assets/Scripts/UI/Weapon/WeaponController.cs
@@ -2,6 +2,8 @@
 using System.Threading;
 using UnityEngine;
 using Cysharp.Threading.Tasks;
+using UnityEngine.AddressableAssets;
+using UnityEngine.ResourceManagement.AsyncOperations;
 
 namespace UI.Weapon
 {
@@ -16,6 +18,11 @@ namespace UI.Weapon
         private WeaponPresenter[] _presenters= new WeaponPresenter[2];
 
         private CancellationTokenSource _cancellationTokenSource;
+        
+        private const string WeaponModelPath = "Assets/Addressables/WeaponModels/";
+        
+        // 武器の最大数は2個
+        private const int MaxWeaponNum = 2;
         
 
         private void Start()
@@ -32,40 +39,83 @@ namespace UI.Weapon
 
             _cancellationTokenSource = new();
             
-            SelectingPresenter.AppearUIs(_cancellationTokenSource.Token).Forget();
-
+            // 武器が選択されるまでループする
             bool isSelected = false;
+            
             // ボタンが押された時はbreak スクロールはプレゼンターを切り替えて再度調べる
             while (true)
             {
-                WeaponActionType actionType = await GetWeaponAction();
-                switch (actionType)
-                {
-                    case WeaponActionType.Press:
-                        Debug.Log("決定");
-                        isSelected = true;
-                        result = WeaponActionType.Press;
-                        break;
-                    case WeaponActionType.Cancel:
-                        Debug.Log("キャンセル");
-                        isSelected = true;
-                        result = WeaponActionType.Cancel;
-                        break;
-                    case WeaponActionType.SelectUp:
-                        Debug.Log("選択上");
-                        break;
-                    case WeaponActionType.SelectDown:
-                        Debug.Log("選択下");
-                        break;
-                }
-
-                if (isSelected)
-                {
-                    break;
-                }
+                // Addressableから武器のモデルを読み込む
+                var targetModel = await Addressables.LoadAssetAsync<WeaponModel>(WeaponModelPath + (_selectedWeaponIndex + 1) + ".asset");
                 
-                _cancellationTokenSource.Cancel();
-                _cancellationTokenSource = new();
+                // 見つかればプレゼンターにモデルをセットして、操作を待つ
+                if (targetModel != null)
+                {
+                    // ビューにモデルを設定していく
+                    SelectingPresenter.SetWeapon(targetModel);
+                    
+                    SelectingPresenter.AppearUIs(_cancellationTokenSource.Token).Forget();
+                    
+                    WeaponActionType actionType = await GetWeaponAction();
+                    
+                    // 操作の結果で分ける
+                    switch (actionType)
+                    {
+                        case WeaponActionType.Press:
+                            Debug.Log("決定");
+                            isSelected = true;
+                            result = WeaponActionType.Press;
+                            break;
+                        case WeaponActionType.Cancel:
+                            Debug.Log("キャンセル");
+                            isSelected = true;
+                            result = WeaponActionType.Cancel;
+                            break;
+                        case WeaponActionType.SelectUp:
+                            Debug.Log("選択上");
+                            _selectedWeaponIndex += 1;
+                            if(_selectedWeaponIndex >= MaxWeaponNum)
+                            {
+                                _selectedWeaponIndex = 0;
+                            }
+                            break;
+                        case WeaponActionType.SelectDown:
+                            Debug.Log("選択下");
+                            _selectedWeaponIndex -= 1;
+                            if(_selectedWeaponIndex < 0)
+                            {
+                                _selectedWeaponIndex = MaxWeaponNum - 1;
+                            }
+                            break;
+                    }
+
+                    if (isSelected)
+                    {
+                        break;
+                    }
+                    
+                    // キャンセレーションを更新する
+                    _cancellationTokenSource.Cancel();
+                    _cancellationTokenSource = new();
+                    
+                    // プレゼンターを切り替える
+                    SelectingPresenter.DisappearUIs(_cancellationTokenSource.Token).Forget();
+
+                    if (isFirst)
+                    {
+                        SelectingPresenter = _presenters[1];
+                        isFirst = false;
+                    }
+                    else
+                    {
+                        SelectingPresenter = _presenters[0];
+                        isFirst = true;
+                    }
+                }
+                else
+                {
+                    throw new Exception("武器のモデルが見つかりませんでした");
+                }
             }
 
             SelectingPresenter.DisappearUIs(_cancellationTokenSource.Token).Forget();
