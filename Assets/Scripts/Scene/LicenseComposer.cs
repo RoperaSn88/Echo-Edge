95f9f123fe04b78c060eb424b02ef5a77ff41b5f
Rope <roperaganmen88@gmail.com>
2026-05-15 00:53:28 +0900

    ライセンス更新

---
+++ Assets/Scripts/Scene/LicenseComposer.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+
+[System.Serializable, CreateAssetMenu(menuName = "License")]
+public class LicenseComposer:ScriptableObject
+{
+    [SerializeField]
+    private string licenseName;
+    
+    public string LicenseName => licenseName;
+    
+    [SerializeField,TextArea]
+    private string licenseIntroduction;
+    public string LicenseIntroduction => licenseIntroduction;
+}
